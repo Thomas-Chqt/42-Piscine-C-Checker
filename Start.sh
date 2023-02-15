ca6f07737a61773dadbe01817916d2afcf6efee5
Thomas Choquet <thomas.publique@icloud.com>
2023-02-15 21:12:48 +0900

    Fix exo7

--- Start.sh
+++ Start.sh
@@ -1,4 +1,4 @@
-EXO=
+EXO=C_
 
 CC=cc
 
@@ -23,7 +23,7 @@ norminette -R CheckForbiddenSourceHeader $REPO_DIR/*/*
 
 echo "\nCompile and run..."
 
-for nbr in 00 01 02 03 04 05 06 07 08 09 10 11 12
+for nbr in 00 01 02 03 04 05 06 07 08
 do
     mkdir $TEMP_DIR
 
